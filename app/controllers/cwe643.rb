5e3bb6912eac3b7b54d9c5476f18d48d3a0a6a5e
Seungcheol Jung <scjung@gtone.co.kr>
2020-06-14 17:55:10 +0900

    CWE-643.

--- app/controllers/cwe643.rb
+++ app/controllers/cwe643.rb
@@ -8,7 +8,19 @@ module Cwe643
 
   def update_cwe643
     doc = Nokogiri::XML(File.open(Rails.root.join('private', 'books.xml')))
-    @result = doc
+    title = params[:title]
+    if params[:defense] == '1'
+      nodes = doc.xpath('//book[./title[contains(text(),$title)]]',
+                        nil, { title: title })
+    else
+      nodes = doc.xpath("//book[./title[contains(text(),'#{title}')]]")
+    end
+    @books = []
+    nodes.each do |node|
+      book = { author: node.xpath('./author/text()'),
+               title: node.xpath('./title/text()') }
+      @books << book
+    end
     render :cwe643
   end
 end
